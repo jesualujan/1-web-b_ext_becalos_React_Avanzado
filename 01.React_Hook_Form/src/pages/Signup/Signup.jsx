c95edb5c2d5131f6e511a04b9fdf5e09a4c88edb
jesualujan <lujan.oficial@gmail.com>
2025-07-08 21:44:13 -0600

    ✨ implementación de useForm & schema de validación

--- 01.React_Hook_Form/src/pages/Signup/Signup.jsx
+++ 01.React_Hook_Form/src/pages/Signup/Signup.jsx
@@ -1,9 +1,52 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import reactLogo from '@/assets/react.svg'
+
+//Definimos el esquema de validación con Yup
+const userFormSchema = yup.object({
+    firstName: yup.string().required('El nombre es obligatorio'),
+    lastName: yup.string().required('El apellido es obligatorio'),
+    dob: yup.date().required('La fecha de nacimiento es obligatoria').max(new Date(), 'La fecha de nacimiento no puede ser mayor a la fecha actual'),
+    email: yup.string().email('Correo electrónico no válido').required('El correo electrónico es obligatorio'),
+    password: yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('La contraseña es obligatoria'),
+  }).required();
 
 const Signup = () => {
+    const navigate = useNavigate();
+
+    // usamos useForm con el esquema de validación de yup
+    const { 
+        register, 
+        handleSubmit,
+        formState: { errors },
+ } = useForm({
+ resolver: yupResolver(userFormSchema)
+ });
+
+ 
+  //Función que maneja el envío del formulario
+  const onSubmit = (data) => {
+    console.log('Datos enviados', data)
+    //redirigimos
+    navigate("/login")
+  }
+
+
   return (
     <div className="max-w-md mx-auto mt-16 p-10 bg-white rounded-xl shadow-xl border border-gray-200">
-    <h2 className="text-4xl font-bold mb-8 text-center text-gray-800">Crear Cuenta</h2>
-    <form className="space-y-6">
+    <h2 className="text-4xl font-bold mb-8 text-center text-gray-800">
+        <span className="inline-flex items-center justify-center gap-2">
+            <img
+            src={reactLogo}
+            alt="React Logo"
+            className="w-10 h-10 animate-spin"
+            />
+        Crear Cuenta
+        </span>
+    </h2>
+    <form onSubmit={handleSubmit(onSubmit)}className="space-y-6">
       <div className="grid grid-cols-2 gap-6">
         <div>
           <label htmlFor="firstName" className="block text-sm font-medium text-gray-600 mb-1">Nombre</label>
@@ -11,8 +54,10 @@ const Signup = () => {
             id="firstName"
             type="text"
             placeholder="Nombre"
+            {...register('firstName')}
             className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition ease-in-out duration-200"
           />
+          {errors.firstName && <p className="text-sm text-red-500">{errors.firstName.message}</p>}
         </div>
         <div>
           <label htmlFor="lastName" className="block text-sm font-medium text-gray-600 mb-1">Apellido</label>
@@ -20,8 +65,10 @@ const Signup = () => {
             id="lastName"
             type="text"
             placeholder="Apellido"
+            {...register('lastName')}
             className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition ease-in-out duration-200"
           />
+          {errors.lastName && <p className="text-sm text-red-500">{errors.lastName.message}</p>}
         </div>
       </div>
   
@@ -30,8 +77,10 @@ const Signup = () => {
         <input 
           id="dob"
           type="date"
+          {...register('dob')}
           className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition ease-in-out duration-200"
         />
+        {errors.dob && <p className="text-sm text-red-500">{errors.dob.message}</p>}
       </div>
   
       <div>
@@ -40,8 +89,10 @@ const Signup = () => {
           id="email"
           type="email"
           placeholder="Correo electrónico"
-          className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition ease-in-out duration-200"
-        />
+          {...register('email')}
+            className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition ease-in-out duration-200"
+          />
+          {errors.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
       </div>
   
       <div>
@@ -50,15 +101,17 @@ const Signup = () => {
           id="password"
           type="password"
           placeholder="Contraseña"
+          {...register('password')}
           className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition ease-in-out duration-200"
         />
+        {errors.password && <p className="text-sm text-red-500">{errors.password.message}</p>}
       </div>
   
       <button 
         type="submit" 
-        className="w-full py-3 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300 transition duration-200"
+        className="w-full py-3 bg-black text-white font-medium rounded-md hover:bg-neutral-800 focus:outline-none focus:ring-2 focus:ring-neutal-600 transition duration-200"
       >
-        Registrarse
+        Crear Cuenta
       </button>
     </form>
   </div>  
