040fc14a71c12845732aab7fa2c485399daea988
jesualujan <lujan.oficial@gmail.com>
2025-07-08 20:02:11 -0600

    ✨ importamos nuestras rutas

--- 01.React_Hook_Form/src/App.jsx
+++ 01.React_Hook_Form/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import AppRoutes from './routes/AppRoutes'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <Navbar />
+     <AppRoutes />
     </>
   )
 }
