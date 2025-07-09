292eb101f72ac7ef33095a95859510db64594d6c
jesualujan <lujan.oficial@gmail.com>
2025-07-08 19:39:50 -0600

    ✨ configuración del alias

--- 01.React_Hook_Form/vite.config.js
+++ 01.React_Hook_Form/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // Habilitar React Fast Refresh para una experiencia de desarrollo más fluida
+    fastRefresh: true,}),],
+    resolve: {
+      alias: {
+        // eslint-disable-next-line no-undef
+        '@': path.resolve(__dirname, './src') // Alias apra importar desde src con @
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], // resuelve estás extensiones automáticamente
+    },
+    server: {
+      port: 3000, // cambia el puerto si quieres (por defecto es 5173)
+      open: true, // abre el navegador automáticamente al iniciar el servidor
+      strictPort: true, // Fallará si el puerto está ocupado (evita que elija otro automáticamente)
+    },
 })
