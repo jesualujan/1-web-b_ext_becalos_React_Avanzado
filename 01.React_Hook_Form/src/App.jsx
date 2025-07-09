a9971f70f39065c1488669ee85986264913a6b30
jesualujan <lujan.oficial@gmail.com>
2025-07-08 21:46:44 -0600

    ✨ localStorage para obtener usuario

--- 01.React_Hook_Form/src/App.jsx
+++ 01.React_Hook_Form/src/App.jsx
@@ -1,13 +1,30 @@
+// Hooks
+import { useState , useEffect} from 'react'
+// Componentes
 import Navbar from './components/Navbar'
 import AppRoutes from './routes/AppRoutes'
+// Estilos
 import './App.css'
 
+
+
 function App() {
+  // Estado para guardar al usuario logueado (null si no hay sesión)
+  const [user, setUser] = useState(null)
+  // useEffect se ejecuta una vez al cargar la app
+  useEffect(() => {
+    // intenta obtener el usuario guardado en el localStorage
+    const dataUser = JSON.parse(localStorage.getItem("user"))
+    // si hay datos, actualiza el estado con ese usuario
+    if(dataUser) setUser(dataUser)
+  },[]) // El array vacío [] significa que se ejecuta solo 1 vez al montar el componente
 
   return (
     <>
-     <Navbar />
-     <AppRoutes />
+    {/* Navbar recibe el usuario actual y la función para actualizarlo */}
+     <Navbar user={user} setUser={setUser}/>
+     {/* AppRoutes recibe el usuario actual y el estado de sesión */}
+     <AppRoutes user={user} setUser={setUser} />
     </>
   )
 }
