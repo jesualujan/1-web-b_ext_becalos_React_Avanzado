ce37042f669e2ed6c05bf4d96c26d0237ac61e0e
jesualujan <lujan.oficial@gmail.com>
2025-07-10 20:36:11 -0600

    ✨ creación Home

---
+++ 02.ReactContext/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import SongList from '../components/SongList/SongList'
+import SongDetails from '../components/SongDetails/SongDetails'
+
+const Home = () => {
+    return (
+        <>
+        <h1>bienvenido al home</h1>
+        <SongList />
+        <SongDetails />        
+        </>
+    )
+}
+
+export default Home;
