012ad57378bdea9178c6463875dad69dc713ea7f
jesualujan <lujan.oficial@gmail.com>
2025-07-10 22:18:26 -0600

    ✨ consumo del contexto

--- 02.ReactContext/src/components/SongDetails/SongDetails.jsx
+++ 02.ReactContext/src/components/SongDetails/SongDetails.jsx
@@ -1,10 +1,22 @@
-
+import {useSongContext} from '@/hook/useSongContext'
 
 const SongDetails = () => {
+    const {selectedSong} = useSongContext()
     return (
-        <div>
-                 Aquí va SongDetails
-        </div>
+      <>
+      {
+        selectedSong.title
+          ? (
+            <div>
+              <img src={selectedSong.img_url} alt={selectedSong.title} />
+              <h4>{selectedSong.title}</h4>
+              <h5>{selectedSong.artist}</h5>
+              <h6>{selectedSong.year}</h6>
+            </div>
+            )
+          : <h1> selecciona una canción...</h1>
+      }
+    </>
     )
 }
 
