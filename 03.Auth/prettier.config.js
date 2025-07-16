f5ecdf19d3fce22b52a36a8b591d9f28dde983ce
jesualujan <lujan.oficial@gmail.com>
2025-07-15 19:53:32 -0600

    ✨ configuración de prettier

---
+++ 03.Auth/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // preferir comillas dobles en los imports 
+    singleQuote: false, //comilla doble por defecto JS
+    jsxSingleQuoute: false, // comillas dobles en componentes JSX
+    trailingComa: "es5", //agrega como al final en objetos/arrays (estándar moderno)
+    semi: true, // asegura punto y coma al final de las lineas
+    arrowParens: "always" // mejora legibilidad en funciones flecha (siempre usa paréntesis)
+}
