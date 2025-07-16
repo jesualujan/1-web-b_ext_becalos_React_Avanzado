2bf3052613639db1b4b61a7d5b2ea613982679d4
jesualujan <lujan.oficial@gmail.com>
2025-07-15 19:53:40 -0600

    ✨ configuración de linter

---
+++ 03.Auth/eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import { defineConfig, globalIgnores } from "eslint/config"
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      // forzamos comillas dobles 
+      "quotes": ["error", "double", { "avoidEscape": true }],
+      //fuerza usar solo minúsculas
+      "react/jsx-pascal-case": ["error", {"allowAllCaps": true,}],
+      // siempre usar punto y coma (en caso de conflicto con prettier)
+    },
+    settings: {
+      react: {
+        version: "detected"
+    }
+  }
+ }
+])
