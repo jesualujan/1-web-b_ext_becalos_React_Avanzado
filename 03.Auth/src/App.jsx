be655e8288c06322f750f498fe668949453fe4a7
jesualujan <lujan.oficial@gmail.com>
2025-07-17 20:58:58 -0600

    ✨ importamos el componente de toastify de manera global

--- 03.Auth/src/App.jsx
+++ 03.Auth/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "@/components/Header"
 import RoutesIndex from "@/routes/RoutesIndex"
+import { ToastContainer } from 'react-toastify'; //importar el contenedor de notificaciones
+import 'react-toastify/dist/ReactToastify.css' // estilos por defecto de Toastify
 
 
 function App() {
@@ -7,6 +9,10 @@ function App() {
 
   return (
     <>
+    {/* El   <ToastContainer /> es el que mostrará las notificaciones en toda la aplicación
+    tienes React Toastify configurado globalment
+    */}
+    <ToastContainer />
     <Header />
      <RoutesIndex />
      </>
