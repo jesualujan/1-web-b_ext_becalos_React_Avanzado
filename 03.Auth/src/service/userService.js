52c084b25eeae0fa4ac6b16e264bb7cd8c974c74
jesualujan <lujan.oficial@gmail.com>
2025-07-17 20:58:03 -0600

    ✨ crear servicios para signup & login

---
+++ 03.Auth/src/service/userService.js
@@ -0,0 +1,24 @@
+// userService es donde simularemos hacer la llamada a la API y para esto samos axios
+// necesitamos pasarle un endpoint para que traiga los datos de la API.
+
+//importamos la instancia configurada
+import axiosInstance from "./axiosConfig"
+
+// ocupo mandar información
+// registar un usuario (Signup)
+const registerUserService = async (data) => {
+    const response = await axiosInstance.post("/register", data)
+    return response; // retornamos toda la respuesta para manejar status
+}
+//http://localhost/register{json}
+
+// autenticar un usuario (Login)
+const loginUserService = async (data) => {
+    const response = await axiosInstance.post("/login", data)
+}
+//http://localhost/login{json}
+
+export {
+    registerUserService,
+    loginUserService
+}
