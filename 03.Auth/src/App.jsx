e841fa31534c29cdfbcc4d844cbc4550363cb56c
jesualujan <lujan.oficial@gmail.com>
2025-07-22 19:44:34 -0600

    ✨ envolvemos nuesta app con el contexto

--- 03.Auth/src/App.jsx
+++ 03.Auth/src/App.jsx
@@ -2,10 +2,9 @@ import Header from "@/components/Header"
 import RoutesIndex from "@/routes/RoutesIndex"
 import { ToastContainer } from 'react-toastify'; //importar el contenedor de notificaciones
 import 'react-toastify/dist/ReactToastify.css' // estilos por defecto de Toastify
-
+import {AuthProvider} from "@/context/AuthContext";
 
 function App() {
- 
 
   return (
     <>
@@ -13,8 +12,10 @@ function App() {
     tienes React Toastify configurado globalment
     */}
     <ToastContainer />
+    <AuthProvider>
     <Header />
      <RoutesIndex />
+    </AuthProvider>
      </>
   )
 }
