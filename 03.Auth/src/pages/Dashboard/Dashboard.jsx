77e6acc4cdeb56ebf23f621a7f05089337fc44af
jesualujan <lujan.oficial@gmail.com>
2025-07-22 21:24:56 -0600

    ✨ creamos una card para el usuario autenticado

--- 03.Auth/src/pages/Dashboard/Dashboard.jsx
+++ 03.Auth/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,53 @@
-
+import { useState, useEffect } from "react";
+import { getMeUserService } from "@/services/userServices";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [userData, setUserData] = useState({}); // vacío
+  const [isLoading, setIsLoading] = useState(true);
 
-export default Dashboard
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const user = await getMeUserService();
+        setUserData(user);
+        console.log("Datos completos del usuario:", user); // 👈 Esto sí está disponible al instante
+      } catch (error) {
+        console.error("Error al obtener usuario", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  if (isLoading) return <p className="text-center mt-10">Cargando perfil...</p>;
+
+  return (
+    <>
+      {/* Creamos una especie de Card */}
+      <div className="max-w-md mx-auto mt-10 bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-xl font-bold text-gray-800 mb-2">
+          Bienvenido a tu perfil
+          {userData.first_name}👋
+        </h2>
+        <p className="text-sm text-gray-500 mb-4">Tu perfil personal:</p>
+
+        <div className="space-y-2 text-gray-700">
+          <p>
+            <span className="font-medium">Nombre:</span> {userData.first_name || "No disponible"}
+          </p>
+          <p>
+            <span className="font-medium">Apellido:</span> {userData.last_name || "No disponible"}
+          </p>
+          <p>
+            <span className="font-medium">Email:</span> {userData.email || "No disponible"}
+          </p>
+          <p>
+            <span className="font-medium">Rol:</span> {userData.role || "No definido"}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Dashboard;
